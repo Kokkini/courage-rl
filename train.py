55ef90d8450e1d068d07642daf987bf99c3d97f0
Kokkini <trannhatquang1104@gmail.com>
2020-08-04 02:23:06 +0000

    add periodical evaluation

--- train.py
+++ train.py
@@ -12,6 +12,7 @@ args = argparse.ArgumentParser()
 args.add_argument("--baseline", action="store_true", help="whether to use the baseline ppo method")
 args.add_argument("--config", help="the config file path")
 args.add_argument("--tune-config", help="config for hyperparamter tuning")
+args.add_argument("--num-tune-runs", type=int, default=20, help="number of hyperparamter sets to test")
 
 args = args.parse_args()
 
@@ -28,8 +29,12 @@ if args.baseline:
 else:
     trainer = CustomPPOTrainer
 
+stop = None
+if "stop" in config:
+    stop = config.pop("stop")
+
 if args.tune_config is None:
-    tune.run(trainer, config=config)
+    tune.run(trainer, config=config, stop=stop)
 else:
     from hyperopt import hp
     from ray.tune.suggest.hyperopt import HyperOptSearch
@@ -39,4 +44,4 @@ else:
     tuning_module = importlib.import_module(f"tuning.{args.tune_config}")
     algo = tuning_module.algo
 
-    tune.run(trainer, config=config, search_alg=algo)
+    tune.run(trainer, config=config, search_alg=algo, stop=stop, num_samples=args.num_tune_runs)
