30300675472cf254c9539d091a5b1a23b2a04378
Quang Nhat Tran <trannhatquang1104@gmail.com>
2020-08-12 12:07:35 -0400

    add images of starting points for environments

--- test.py
+++ test.py
@@ -1,9 +1,12 @@
 from envs.dangerous_maze_env import DangerousMazeEnv
 import numpy as np
+from PIL import Image
 
 config = {"level_file": "envs/6x6.txt", "death_penalty": True}
 env = DangerousMazeEnv(config)
-
+img = env.render()
+img = Image.fromarray(img)
+img.save("media/6x6.jpg")
 for i in range(10000):
     state, reward, done, info = env.step(np.random.randint(4))
     print(reward)
@@ -11,3 +14,4 @@ for i in range(10000):
     if done:
         env.reset()
         env.render()
+
